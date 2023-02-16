a16b7f8535b60a75a627234700fc1aec689f52b3
Mikanixonable <47976119+Mikanixonable@users.noreply.github.com>
2023-02-16 20:34:37 +0900

    色分け

--- koushin.py
+++ koushin.py
@@ -2,6 +2,7 @@ import os.path
 import datetime
 import json
 import glob
+import math
 
 #htmlファイル名全取得
 files = glob.glob("*.html")
@@ -11,13 +12,14 @@ dates=[] #{cdates:2023brbr, madtes:2023brbr}
 for i in range(len(files)):
     path = files[i]
     #UnixTimeStamp取得
-    ctime = os.path.getctime(path)
-    mtime = os.path.getmtime(path)
+    ctime = math.floor(os.path.getctime(path))
+    mtime = math.floor(os.path.getmtime(path))
     #日付化
-    cdate = datetime.datetime.fromtimestamp(ctime).replace(microsecond=0)
-    mdate = datetime.datetime.fromtimestamp(mtime).replace(microsecond=0)
+    cdate = datetime.datetime.fromtimestamp(ctime).date()
+    mdate = datetime.datetime.fromtimestamp(mtime).date()
 
     date_dic = {
+    "name":"_"+str(files[i]).replace(".",""),
     "ctime":str(ctime),
     "mtime":str(mtime),
     "cdate":str(cdate),
@@ -27,18 +29,17 @@ for i in range(len(files)):
     # dates.append(mtime)
 
 #キーの拡張子を取り除き先頭に_をつける
-for i in range(len(files)):
-    files[i] = os.path.splitext(files[i])[0]
-    files[i]="_"+files[i]
+# for i in range(len(files)):
+#     files[i] = os.path.splitext(files[i])[0]
+#     files[i]="_"+files[i]
 ##########################################################
 
 
 ##########################################################
 
-dict1 = dict(zip(files,dates))
-# dict2 = dict(zip(files,dates))
+# dict1 = dict(zip(files,dates))
 
 json1 = open('./meta.json', mode="w")
-json.dump(dict1, json1)
+json.dump(dates, json1)
 
 json1.close()
