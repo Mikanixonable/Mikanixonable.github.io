07075d6c940bd42a97d7011d4842b848cd4d6b5f
Mikanixonable <47976119+Mikanixonable@users.noreply.github.com>
2023-02-20 07:29:57 +0900

    a
    a
    
    a
    
    a
    
    a
    
    a
    
    a

--- koushin.py
+++ koushin.py
@@ -3,43 +3,55 @@ import datetime
 import json
 import glob
 import math
+import bs4
+from html.parser import HTMLParser
+import codecs
 
 #htmlファイル名全取得
 files = glob.glob("*.html")
 
-#作成日時更新日時の取得、リスト化
-dates=[] #{cdates:2023brbr, madtes:2023brbr}
+#ファイルの情報の取得、リスト化
+dates=[]
 for i in range(len(files)):
     path = files[i]
     #UnixTimeStamp取得
     ctime = math.floor(os.path.getctime(path))
     mtime = math.floor(os.path.getmtime(path))
-    #日付化
+
+    #UnixTimeStampの日付化
     cdate = datetime.datetime.fromtimestamp(ctime).date()
     mdate = datetime.datetime.fromtimestamp(mtime).date()
 
+    #タイトルの取得、タイトルがなければファイル名を入れる
+    soup = bs4.BeautifulSoup(open(path, mode= "r",encoding="utf-8"), 'html.parser')
+    try: 
+        title = str(soup.title.string)
+    except AttributeError:
+        title = "untitled"
+
+    try: 
+        content = soup.meta.get("content")
+        
+    except AttributeError:
+        content = ""
+    
+
+    print(content)
+
+
     date_dic = {
     "name":"_"+str(files[i]).replace(".",""),
+    "title":title,
+    "content":content,
     "ctime":str(ctime),
     "mtime":str(mtime),
     "cdate":str(cdate),
     "mdate":str(mdate),
     }
     dates.append(date_dic)
-    # dates.append(mtime)
-
-#キーの拡張子を取り除き先頭に_をつける
-# for i in range(len(files)):
-#     files[i] = os.path.splitext(files[i])[0]
-#     files[i]="_"+files[i]
-##########################################################
 
 
-##########################################################
-
-# dict1 = dict(zip(files,dates))
-
-json1 = open('./meta.json', mode="w")
-json.dump(dates, json1)
+json1 = codecs.open('./meta.json', "w", "utf-8")
+json.dump(dates, json1, indent = 2, ensure_ascii=False)
 
 json1.close()
