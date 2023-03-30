c448e3540cd635cf7d9dd7667f5d48d964868c35
Mikanixonable <47976119+Mikanixonable@users.noreply.github.com>
2023-03-31 00:00:26 +0900

    s

--- 1.py
+++ 1.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 
-nums = [10,11,12,13,14,15,16,17,18,19,20,23,31,39,43,44]
+# nums = [10,11,12,13,14,15,16,17,18,19,20,23,31,39,43,44]
+nums = [4,49,41,54,120,121,122,123,123,125,126,127,128,129,130]
+genre = "font"
 names = map(lambda n: str(n)+".html",nums)
 for name in names:
     with open(name, mode='rt', encoding='utf-8') as f:
@@ -14,11 +16,11 @@ for name in names:
 
 
     if soup.find("meta") is None:
-        meta = soup.new_tag("meta",genre="illust")
+        meta = soup.new_tag("meta",genre=genre)
         soup.find("head").append(meta)
     else:
-        soup.find("meta")["genre"]="illust"
-    del soup.find("meta")["tag"]
+        soup.find("meta")["genre"]=genre
+
 
 
     with open(name, mode='w',encoding='utf-8') as f:
