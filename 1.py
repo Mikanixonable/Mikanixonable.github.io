6f47f498b0cc2081949308ca3b2e45c0c23539b8
Mikanixonable <47976119+Mikanixonable@users.noreply.github.com>
2023-04-01 20:32:46 +0900

    d

--- 1.py
+++ 1.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 
 # nums = [10,11,12,13,14,15,16,17,18,19,20,23,31,39,43,44]
-nums = [4,49,41,54,120,121,122,123,123,125,126,127,128,129,130]
-genre = "font"
+# nums = [4,49,41,54,120,121,122,123,123,125,126,127,128,129,130]
+# nums = [80,81,82,83,84,85,86,87,88,89] #math
+# nums = [22,23,27,32,33,34,35,36] #3d
+nums = [22,23,27,32,33,34,35,36] #3D
+genre = ""
 names = map(lambda n: str(n)+".html",nums)
 for name in names:
     with open(name, mode='rt', encoding='utf-8') as f:
